28862e847554bc7a33cedbe68e8bc5c6ed8c90a1
Indu Prasad <indu.prasad@tredence.com>
2023-09-08 11:56:37 +0530

    SSO_MS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
+
+// Bootstrap components
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+/**
+ * We recommend wrapping most or all of your components in the MsalProvider component. It's best to render the MsalProvider as close to the root as possible.
+ */
+ root.render(
   <React.StrictMode>
-    <App />
+      <MsalProvider instance={msalInstance}>
+          <App />
+      </MsalProvider>
   </React.StrictMode>
 );
 
